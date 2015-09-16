4fa12a5a1d5bb4964233e53a313c2c665e983b08
David Mercier <david.mercier@cdlinc.ca>
2015-09-15 22:44:40 -0400

    Fonctionnel!
    Fonctionnel!

--- calculator.js
+++ calculator.js
@@ -1,3 +1,109 @@
 /**
  * Created by dmercier on 2015-09-15.
  */
+
+var Calculator = function (number) {
+    this.number = number || 0;
+    this.tracesActivated = false;
+    console.log("Calculator constructor");
+
+    this.enableTraces = function() {
+        this.tracesActivated = true;
+        return this;
+    }
+
+    this.disableTraces = function() {
+        this.tracesActivated = false;
+        return this;
+    }
+
+    this.log = function(stringToLog) {
+        if(this.tracesActivated == true) {
+            console.log(stringToLog);
+        }
+        return this;
+    }
+
+    this.reset = function() {
+        this.number = 0;
+        return this;
+    }
+
+    this.set = function(number) {
+        this.number = number;
+        return this;
+    }
+
+    this.getValue = function() {
+        this.log("getValue " + this.number);
+        return this.number;
+    }
+
+    this.add = function(numberToAdd) {
+        var tmpNumber = this.number;
+        this.number += numberToAdd;
+        this.log("add " + tmpNumber + " + " + numberToAdd + " = " + this.number);
+        return this;
+    };
+
+    this.substract = function(numberToSubstract) {
+        var tmpNumber = this.number;
+        this.number -= numberToSubstract;
+        this.log("substract " + tmpNumber + " - " + numberToSubstract + " = " + this.number);
+        return this;
+    };
+
+    this.multiply = function(multiplyFactor) {
+        var tmpNumber = this.number;
+        this.number *= multiplyFactor;
+        this.log("multiply " + tmpNumber + " by " + multiplyFactor + " = " + this.number);
+        return this;
+    };
+
+    this.divide = function(divideFactor) {
+        var tmpNumber = this.number;
+        this.number /= divideFactor;
+        this.log("divide " + tmpNumber + " by " + divideFactor + " = " + this.number);
+        return this;
+    };
+
+    this.sin = function() {
+        this.number = Math.sin(this.number);
+        return this;
+    };
+
+    this.cos = function() {
+        this.number = Math.cos(this.number);
+        return this;
+    };
+
+    this.tan = function() {
+        this.number = Math.tan(this.number);
+        return this;
+    };
+
+    this.factorial = function() {
+        this.number = _factorial(this.number);
+        return this;
+    };
+
+    // this function is private
+    isInt = function(n) {
+        return n % 1 === 0;
+    }
+
+    // factorial taken from http://jsfiddle.net/glenn_antoine/fkCyJ/
+    // this function is private
+    _factorial = function(num) {
+        if(isInt(num) == false) {
+            return 0;
+        }
+        else if(num == 0) {
+            return 1;
+        }
+        else {
+            return (num * _factorial(num - 1));
+        }
+    }
+};
+
--- main.js
+++ main.js
@@ -21,12 +21,31 @@ var LogBadResult = function(functionName, stringToDisplay) {
 
 var calc = new Calculator(10);
 //calc.enableTraces();
+
+calc.add
+
 LogResult(calc.getValue() == 10, "getValue", "Test getValue()");
-LogResult(calc.add(12) == 22, "add", "Test add(12)");
-LogResult(calc.substract(12) == 10, "substract", "Test substract(12)");
-LogResult(calc.multiply(2) == 20, "multiply", "Test multiply(2)");
-LogResult(calc.divide(2) == 10, "divide", "Test divide(2)");
-LogResult(calc.sin() == Math.sin(calc.getValue()), "sin", "Test sin(" + calc.getValue() + ")");
-LogResult(calc.cos() == Math.cos(calc.getValue()), "cos", "Test cos(" + calc.getValue() + ")");
-LogResult(calc.tan() == Math.tan(calc.getValue()), "tan", "Test tan(" + calc.getValue() + ")");
-LogResult(calc.factorial() ==  3628800, "factorial", "Test factorial(" + calc.getValue() + ")");
+LogResult(calc.add(12).getValue() == 22, "add", "Test add(12)");
+LogResult(calc.substract(12).getValue() == 10, "substract", "Test substract(12)");
+LogResult(calc.multiply(2).getValue() == 20, "multiply", "Test multiply(2)");
+LogResult(calc.divide(2).getValue() == 10, "divide", "Test divide(2)");
+LogResult(calc.reset().getValue() == 0, "reset", "Test reset()");
+LogResult(calc.set(10).getValue() == 10, "set", "Test set(10)");
+LogResult(calc.sin().getValue() == Math.sin(10), "sin", "Test sin(" + calc.getValue() + ")");
+calc.set(10);
+LogResult(calc.cos().getValue() == Math.cos(10), "cos", "Test cos(" + calc.getValue() + ")");
+calc.set(10);
+LogResult(calc.tan().getValue() == Math.tan(10), "tan", "Test tan(" + calc.getValue() + ")");
+LogResult(calc.factorial().getValue() ==  0, "factorial", "Test factorial(" + calc.getValue() + ")");
+calc.set(10);
+LogResult(calc.factorial().getValue() ==  3628800, "factorial", "Test factorial(" + calc.getValue() + ")");
+calc.reset();
+LogResult(calc.add(10).substract(5).getValue() == 5, "Chained #1", "Test chain #1");
+LogResult(calc.add(95).divide(2).multiply(4).getValue() == 200, "Chained #2", "Test chain #2");
+calc.reset();
+calc.set(1);
+LogResult(calc.sin().getValue() == Math.sin(1), "sin", "Test sin(" + calc.getValue() + ")");
+calc.set(1);
+LogResult(calc.cos().getValue() == Math.cos(1), "cos", "Test cos(" + calc.getValue() + ")");
+calc.set(1);
+LogResult(calc.tan().getValue() == Math.tan(1), "tan", "Test tan(" + calc.getValue() + ")");
